3683c34975ba5ed3611de00864d0d470b47d0a11
chrissyhunt87 <chrissy.hunt@gmail.com>
2018-07-09 19:31:34 -0400

    Added Chrissy to names array

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import NewComponent from './components/new-components/newcomponent';
 class App extends Component {
 
   render() {
-    let names = ["Justin"]
+    let names = ["Justin", "Chrissy"]
     let profiles = names.map((name)=> <Profile name={name}/>)
     return (
       <div className="App">
